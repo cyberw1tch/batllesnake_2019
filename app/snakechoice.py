8aefeab412ae79cc9a2d643d359429376f227e9d
Lancaster <lncstr@mail.ru>
2018-03-03 12:44:26 -0800

    hahahahaha

---
+++ app/snakechoice.py
@@ -0,0 +1,7 @@
+def choice (data):
+    # if health < whatever we're worried about AND snake is of even length:
+        # chase tail
+    # else
+        # chase food!
+
+    return #up, down, left, right, w/e
