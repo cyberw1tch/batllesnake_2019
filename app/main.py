4e3e07ca0ce1e046a2591d259df7b8b63569055f
Marcus Merryfield <marcusmerryfield@gmail.com>
2018-03-03 14:46:05 -0800

    main
    asdfadsf

--- app/main.py
+++ app/main.py
@@ -31,10 +31,7 @@ def start():
         'color': '#CACFFF',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'Mariah Scarey',
-        'head_type': 'tongue',
-    	'tail_type': 'fat-rattle',
-    	'taunt': 'All I want for Christmas is your life'
+        'name': 'Mariah Scarey'
     }
 
 
