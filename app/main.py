64351f6e5a486884b0600bf3cd3ebd1fb513c722
Marcus Merryfield <marcusmerryfield@gmail.com>
2018-03-03 14:28:29 -0800

    fix main
    awefawf

--- app/main.py
+++ app/main.py
@@ -31,9 +31,9 @@ def start():
         'color': '#CACFFF',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'Mariah Scarey'
+        'name': 'Mariah Scarey',
         "head_type": "tongue",
-    	"tail_type": "fat-rattle"
+    	"tail_type": "fat-rattle",
     	"taunt": "All I want for Christmas is your life"
     }
 
