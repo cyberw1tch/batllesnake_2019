9b6770e74efbd21788b248e83f11f8b8a9afae3f
Shmazool <yousiefc@gmail.com>
2018-03-03 14:13:07 -0800

    working board for testing

--- app/gametocode.py
+++ app/gametocode.py
@@ -1,16 +1,24 @@
 import numpy as np
 
 
-def main():
+#def main():
 
-    sneks = [[(0,0),(0,1)][(9,3),(9,4)]]
+    #sneks = [[(0,0),(0,1)],[(9,3),(9,4)]]
+    #snack = [(5,4)(3,3)]
 
-    tonumpy(sneks,10,10)
+    #tonumpy(snack,sneks,10,10)
 
-def tonumpy (enemies ,width, height):
-    
+def tonumpy (enemies ,food, width, height):
+
     code_board = np.zeros((height,width), dtype=int)
-    print(code_board)
+    #print(code_board)
 
-if __name__=="__main__":
-    main()
+    for block in enemies:
+        for coord in block:
+            code_board[coord[0]][coord[1]] = 1
+
+    return code_board,food[0],enemies[0][0]
+    #print(code_board)
+
+#if __name__=="__main__":
+    #main()
