4be40971e7a49858ee724dfd50f984f65ed8c4f2
Marcus Merryfield <marcusmerryfield@gmail.com>
2018-03-03 13:32:45 -0800

    parsesnakes fix
    fix

--- app/parsesnakes.py
+++ app/parsesnakes.py
@@ -5,13 +5,9 @@ def parsesnakes(data):
 		x = [snake['body']['data'][i]['x'] for i in range(snake['length'])]
 		y = [snake['body']['data'][i]['y'] for i in range(snake['length'])]
 
-<<<<<<< HEAD
 		if snake == data['you']:
 			snakes.insert(0,list(zip(y,x)))
 		else:
 			snakes.append(list(zip(y,x)))
 
 	return snakes
-=======
-	list(zip(y,x))
->>>>>>> 5f24b044f623fcb0d947859f6f581ef32467d0da
