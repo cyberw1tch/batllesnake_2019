35eabfd362c35c8aa72aabbad69c0bda5374e590
Marcus Merryfield <marcusmerryfield@gmail.com>
2018-03-03 16:52:53 -0800

    aesthetics
    YAY

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#CACFFF',
+        'color': '#FFAE00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'Mariah Scarey',
